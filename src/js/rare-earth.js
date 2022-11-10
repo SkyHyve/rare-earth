108209263595415f4f119f03330752491bdbe153
Matthew Graham <matthew.grahan@skyhyve.com.au>
2022-11-10 15:45:49 +1100

    btn-dark

--- src/js/rare-earth.js
+++ src/js/rare-earth.js
@@ -373,7 +373,7 @@ const RareEarth = {
     )
   },
   ExportWidget: function(props){
-    return(<button onClick={props.exportTable}>Export</button>)
+    return(<button className="btn btn-dark" onClick={props.exportTable}>Export</button>)
   },
   Table: React.forwardRef(function(props, ref){
     RareEarth.validateProps(props);
